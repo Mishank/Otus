7bc4a58b2c98711fa96a2a31c02fecb522e51689
Misha Komarov <Misha140699@mail.ru>
2018-08-31 13:42:29 +0300

    Add lesson 3

---
+++ Home Work L-3/src/main/java/ru/otus/ObjSizeService.java
@@ -0,0 +1,34 @@
+package ru.otus;
+
+import java.util.function.Supplier;
+
+public class ObjSizeService {
+
+    // Количество прогонов программы
+    private final int repSize = 1;
+
+    // Количество создаваемых объектов
+    private final int objsSize = 2_000_000;
+
+
+    public long calculate(Supplier<?> obj) throws InterruptedException {
+
+        Runtime runtime = Runtime.getRuntime();
+
+        long result = 0;
+
+        for (int i = 0; i < repSize; i++) {
+
+            Object[] array = new Object[objsSize];
+
+            for (int j = 0; j < objsSize; j++) {
+                array[j] = obj.get();
+            }
+
+            System.gc();
+            Thread.sleep(1000);
+            result += (runtime.totalMemory() - runtime.freeMemory()) / objsSize;
+        }
+        return result / repSize;
+    }
+}
